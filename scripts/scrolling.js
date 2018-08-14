42ca120ed48998aea0f24a15232abab1cbbea902
Evhenii Melnyk <melnik.zheka@gmail.com>
2018-08-14 15:15:24 +0300

    up and down buttons

--- scripts/scrolling.js
+++ scripts/scrolling.js
@@ -1,7 +1,7 @@
 $(function(){
     $('a[data-target^="anchor"]').on('click', function(){
         var target = $(this).attr('href'), 
-            bl_top = $(target).offset().top;
+            bl_top = $(target).offset().top-50;
         $('body, html').animate({scrollTop: bl_top}, 700);
         return false;
     });
