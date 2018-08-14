a924ba57ff3427c58cb8deebe74a605b28dce7ca
Evhenii Melnyk <melnik.zheka@gmail.com>
2018-08-14 13:41:39 +0300

    fixed scrolling, contact form without social networks

--- scripts/scrolling.js
+++ scripts/scrolling.js
@@ -1,6 +1,5 @@
 $(function(){
-
-    $('a[data-target^="anchor"]').bind('click.smoothscroll'), function(){
+    $('a[data-target^="anchor"]').on('click', function(){
         var target = $(this).attr('href'), 
             bl_top = $(target).offset().top;
         $('body, html').animate({scrollTop: bl_top}, 700);
