7f3e46e00ac599e12645703899678d144e596e12
Evhenii Melnyk <melnik.zheka@gmail.com>
2018-08-15 11:17:48 +0300

    portfolio buttons, gradient, and other little changes

---
+++ scripts/fixed_header.js
@@ -0,0 +1,10 @@
+// $(document).ready(function(){
+//     $("header").removeClass("default"); // Удаляем класс при загрузке страницы
+//     $(window).scrolling(function(){//Функция вычесления на сколько мы проскролили страницу
+//         if ($(this).scrollTop() > 20){ //Если скролл элемента хедер > 20px 
+//             $("header").addClass("default").fadeIn('fast'); // Добавляем хедеру класс дефолт с эффектом "фаст"
+//         } else{
+//             $("header").removeClass("default").fadeIn('fast');
+//         };
+//     });
+// });
